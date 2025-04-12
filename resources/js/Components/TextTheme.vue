27865bce30a02845700f765db20e0bec096a6adf
Q300Z <Q300Zhomas@gmail.com>
2025-04-12 18:13:54 +0000

    fix(ci): Ajout de l'installation des deps node + build

--- resources/js/Components/TextTheme.vue
+++ resources/js/Components/TextTheme.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-export type TextThemeVariant = 'primary' | 'secondary' | 'tertiary';
-const props = defineProps({
-  variant: { type: TextThemeVariant, default: 'primary' },
-})
+import { computed } from 'vue';
 
-const textThemeClasses = {
+export type TextThemeVariant = 'primary' | 'secondary' | 'tertiary';
+
+const props = defineProps<{
+  variant?: TextThemeVariant; // Utilisation du type TextThemeVariant
+}>();
+
+const textThemeClasses: Record<TextThemeVariant, string> = {
   primary: 'text-black dark:text-white',
   secondary: 'text-gray-700 dark:text-gray-400',
   tertiary: 'text-gray-500 dark:text-gray-600'
-}
+};
+
+// Utilisation d'une valeur par défaut pour éviter les erreurs de type
 const textThemeClass = computed(() => {
-  return textThemeClasses[props.variant]
-})
+  return textThemeClasses[props.variant || 'primary' as TextThemeVariant]; // Cast pour éviter l'erreur de type
+});
 </script>
 
 <template>
