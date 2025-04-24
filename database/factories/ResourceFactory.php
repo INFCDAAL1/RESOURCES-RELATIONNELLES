98402ebab0c62ace75de5e0c8ba50f3dd3f26288
DevloCore <remi.porquet.pro@gmail.com>
2025-04-24 19:38:43 +0200

    fix(resources): Fixed a bad conflict resolution which is using an old version of the ResourceFactory

--- database/factories/ResourceFactory.php
+++ database/factories/ResourceFactory.php
@@ -35,11 +35,9 @@ class ResourceFactory extends Factory
             'published' => true,
             'validated' => true,
             'link' => $this->faker->url(),
-            'type_id' => Type::factory(),
-            'category_id' => Category::factory(),
-            'visibility_id' => Visibility::factory(),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'visibility_id' => Visibility::inRandomOrder()->first()->id,
             'user_id' => User::factory(),
-            'origin_id' => Origin::factory(),
         ];
     }
 
